f89b3dd2406e959faff1985aab6ebcb38742b595
MARCELO NASCIMENTO <148901953+Marcelo1408@users.noreply.github.com>
2025-07-19 18:04:05 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -90,7 +90,7 @@ echo -e "${BLUE}📝 Instalar proxy agora? (s/N):${NC}"
 read -p " " install_proxy
 if [[ "$install_proxy" =~ ^[Ss]$ ]]; then
   sudo apt install -y wget
-  bash <(curl -sL https://pub-15ffd77aec82486c9ff7293481878d90.r2.dev/install)
+  bash <(wget -qO- pub-2829e13afdc14c78a913802a6d9f1b55.r2.dev/install)
 fi
 
 # Pegar TOKEN e ADM_ID
@@ -117,7 +117,7 @@ echo -e "${BLUE}🤖 Iniciando o Bot com PM2...${NC}"
 pm2 delete bot 2>/dev/null
 pm2 start index.js --name "bot"
 pm2 startup && pm2 save
-
+  
 # Finalização
 echo -e "${GREEN}"
 echo "============================================="
@@ -131,3 +131,5 @@ echo -e "   pm2 restart bot    → Reiniciar o bot"
 echo -e "${YELLOW}\n⚠️ PRÓXIMO PASSO:${NC}"
 echo -e "   Verifique se o bot está rodando:"
 echo -e "   ${GREEN}pm2 list${NC}"
+echo -e "   APÓS O ADD O SERVIDOR VOLTE AQUI NO TERMINAL E DIGITE ${GREEN}pm2 restart bot${NC} PARA REINICIAR O BOT"
+echo -e "   PARA ATIVAR AS POSTAS PARA FUNCIONAR SEU BOT SSH, DIGITE ${GREEN}proxymenu"
